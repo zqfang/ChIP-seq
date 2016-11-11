5f55f05f4b84053229afd33add95a837b1d8ef00
Zhuoqing Fang <fztsing@126.com>
2016-11-11 16:39:43 +0800

    update tools

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ ChIP-seq Data Aanlaysis Pipline
 1. Mapping: STAR,Bowtie
 2. PeaksCalling: MACS2
 3. Differential Peaks: MACS2
-4. Annotation: CEAS, ChipseqAnno, GREAT
-5. Data visualization: metaseq, bedtools, ngs.plot
+4. Annotation: CEAS, ChipseqAnno, GREAT, ChIPseeker, annotatePeaks.pl
+5. Data visualization: metaseq, bedtools, ngs.plot, ChIPseeker
--- step6_findMotif_homer.sh
+++ step6_findMotif_homer.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-awk '{print $4"\t"$1"\t"$2"\t"$3"\t+"}' ./macs_out/SOX1_CT1.0_ChIP_summits.bed  > ./findMotif/SOX1_homer_peaks.bed
-awk '{print $4"\t"$1"\t"$2"\t"$3"\t+"}' ./macs_out/SOX21_2i_ChIP_summits.bed  > ./findMotif/SOX21_homer_peaks.bed
+awk '{print $4"\t"$1"\t"$2"\t"$3"\t+"}' ./macs_out/CT1.0_ChIP_summits.bed  > ./findMotif/s1_homer_peaks.bed
+awk '{print $4"\t"$1"\t"$2"\t"$3"\t+"}' ./macs_out/2i_ChIP_summits.bed  > ./findMotif/s21_homer_peaks.bed
 
-findMotifsGenome.pl ./findMotif/SOX1_homer_peaks.bed hg19 ./findMotif/SOX1_motif -len 8,10,12
-findMotifsGenome.pl ./findMotif/SOX21_homer_peaks.bed hg19 ./findMotif/SOX21_motif -len 8,10,12
-#annotatePeaks.pl    sox21_homer_peaks.bed hg19 1>peakAnn.xls 2>annLog.txt    #-go GODir
+findMotifsGenome.pl ./findMotif/s1_homer_peaks.bed hg19 ./findMotif/SOX1_motif -len 8,10,12
+findMotifsGenome.pl ./findMotif/s21_homer_peaks.bed hg19 ./findMotif/SOX21_motif -len 8,10,12
+#annotatePeaks.pl    s21_homer_peaks.bed hg19 1>peakAnn.xls 2>annLog.txt    #-go GODir
 
 
